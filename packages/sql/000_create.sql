c9446ae42c3902e01168dc0fe0e9b4bf9886f11f
AmbatiSathwik <ambatisathwil2311@gmail.com>
2021-07-21 10:12:59 +0530

    contains all sql files of project first commit

---
+++ packages/sql/000_create.sql
@@ -0,0 +1,17 @@
+drop table if exists notes;
+drop table if exists details;
+CREATE TABLE details(
+    id serial PRIMARY KEY,
+    username TEXT,
+    email TEXT,
+    username TEXT,
+    updated date,
+    password VARCHAR(20)
+);
+
+CREATE TABLE notes(
+    notes TEXT,
+    usr INTEGER,
+    time date,
+    FOREIGN KEY (usr) REFERENCES details(id)
+);
