cb225488c9b6843a50d320fcc603441b29ff1c17
AmbatiSathwik <ambatisathwil2311@gmail.com>
2021-07-24 10:50:03 +0530

    added home function

--- packages/__init__.py
+++ packages/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template
 from .auth import auth
 from . import db
 
@@ -12,4 +12,7 @@ def create_app():
     app.config.from_object(__name__)
     app.register_blueprint(auth, url_prefix='/')
     db.init_app(app)
+    @app.route('/')
+    def home():
+    	return render_template("home.html")
     return app
